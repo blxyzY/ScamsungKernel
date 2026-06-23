7e7bf6addc65d509a757a54f4c65f963b922c34b
bzett <89435378+blxyzY@users.noreply.github.com>
2026-06-23 21:07:34 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-export TC=/home/vigus/zyc-clang
+        export TC=$HOME/clang-11
+        export GCC=$HOME/gcc
 
-export CROSS_COMPILE=$TC/bin/aarch64-linux-gnu-
-export LD=$TC/bin/ld.lld
-export OBJCOPY=$TC/bin/llvm-objcopy
-export AS=$TC/bin/llvm-as
-export NM=$TC/bin/llvm-nm
-export STRIP=$TC/bin/llvm-strip
-export OBJDUMP=$TC/bin/llvm-objdump
-export READELF=$TC/bin/llvm-readelf
-export CC=$TC/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
-#export ANDROID_MAJOR_VERSION=r
+        # Setup Compiler & Toolchain Path
+        export CROSS_COMPILE=$GCC/bin/aarch64-linux-android-
+        export CROSS_COMPILE_ARM32=$GCC/bin/arm-linux-androideabi-
+        export LD=$TC/bin/ld.lld
+        export OBJCOPY=$TC/bin/llvm-objcopy
+        export AS=$TC/bin/llvm-as
+        export NM=$TC/bin/llvm-nm
+        export STRIP=$TC/bin/llvm-strip
+        export OBJDUMP=$TC/bin/llvm-objdump
+        export READELF=$TC/bin/llvm-readelf
+        export CC=$TC/bin/clang
+        export CLANG_TRIPLE=aarch64-linux-gnu-
+        export ARCH=arm64
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+        export KCFLAGS=-w
+        export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean && make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper
-clear
+        # Clean up
+        make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean && make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper
+        clear
 
-make -s -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/a23_eur_open_defconfig
-make -s -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
+        # Configure & Build
+        make -s -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/a23_open_eur_defconfig
+        make -s -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc)
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+        # Copy Output
+        cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
